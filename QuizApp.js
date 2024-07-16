b2d63026b069b8de2bdf8d05793a4bbf4e47dbac
Rupinder-17 <rupinderrandhey90@gmail.com>
2024-07-16 21:32:06 +0530

    target dom

--- QuizApp.js
+++ QuizApp.js
@@ -25,3 +25,39 @@ const questions = [
     answer: "100°C",
   },
 ];
+
+
+ let Questionindex = 0;
+ let score = 0;
+ let quizUsers = JSON.parse(localStorage.getItem("quizUser")) || [];
+
+ if (localStorage.getItem("users")) {
+   quizUsers = JSON.parse(localStorage.getItem("users"));
+ }
+
+ if (localStorage.getItem("score")) {
+   score = parseInt(localStorage.getItem("score"));
+ }
+
+ const inputid = document.getElementById("input");
+ const inputName = document.getElementById("userName");
+ const submitButton = document.getElementById("submitbtn");
+ const mainContainer = document.getElementById("container");
+ const SecondContainer = document.getElementById("SecondContainer");
+ const scoreContainer = document.getElementById("scoreContainer");
+ const head1 = document.querySelector("h1");
+ const emoji = document.getElementById("emoji");
+ const RestartButton = document.getElementById("restart");
+ const scoreBoard = document.getElementById("score-board");
+ const BackToStart = document.getElementById("Back-to-start");
+ const userscoreBoard = document.getElementById("userScoreBoard");
+ const scoreButton = document.getElementById("scorebutton");
+ const scorebutton1 = document.getElementById("scorebutton1");
+
+ scoreButton.addEventListener("click", userScoreBoard);
+ scorebutton1.addEventListener("click", userScoreBoard);
+
+ // backGround color
+ const byDefaultColor = "bg-blue-100";
+ const correctAnswerColor = "bg-green-700";
+//  const incorrectAnswerColor = "bg-red-500";
