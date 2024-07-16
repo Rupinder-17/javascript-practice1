9f73762d9981ad892166ae0292c771cfca026ea8
Rupinder-17 <rupinderrandhey90@gmail.com>
2024-07-16 14:54:34 +0530

    create html and js file

---
+++ QuizApp.js
@@ -0,0 +1,27 @@
+const questions = [
+  {
+    question: "What is the capital of France? 🇫🇷",
+    options: ["Berlin", "Madrid", "Paris", "Rome"],
+    answer: "Paris",
+  },
+  {
+    question: "Which planet is known as the Red Planet? 🪐",
+    options: ["Earth", "Mars", "Jupiter", "Saturn"],
+    answer: "Mars",
+  },
+  {
+    question: "What is the largest ocean on Earth? 🌊",
+    options: ["Atlantic", "Indian", "Arctic", "Pacific"],
+    answer: "Pacific",
+  },
+  {
+    question: "Which country is famous for sushi? 🍣",
+    options: ["China", "Japan", "Korea", "Thailand"],
+    answer: "Japan",
+  },
+  {
+    question: "What is the boiling point of water? 💧",
+    options: ["90°C", "100°C", "110°C", "120°C"],
+    answer: "100°C",
+  },
+];
