f10c0361423f652c75db43dbdbfa2926c2a201f2
Rupinder-17 <rupinderrandhey90@gmail.com>
2024-12-22 21:12:13 +0530

    try to create chatbox

---
+++ practice/config.js
@@ -0,0 +1,5 @@
+const SENDBIRD_CONFIG = {
+  APP_ID: "72677D22-93C1-4D32-A646-B2F330DE881F", // Replace with your Sendbird Application ID
+  API_TOKEN: "4f5cfcae47108d35abc13afdcf26d2865ddd8508", // Replace with your Sendbird API Token
+  API_ENDPOINT: "https://api-72677D22-93C1-4D32-A646-B2F330DE881F.sendbird.com",
+};
